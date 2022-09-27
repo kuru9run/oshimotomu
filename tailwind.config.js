0c1393bffd30bf59fbf5996b6de4f46a7d97f129
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-09-27 16:45:44 +0900

    daisyUIのテーマ作ってナビバー設置

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,22 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#63A1D9",
+          "secondary": "#FCE58C",
+          "accent": "#EEAF3A",
+          "neutral": "#FF8991",
+          "base-100": "#f3f4f6",
+          "info": "#5eead4",
+          "success": "#36D399",
+          "warning": "#6b7280",
+          "error": "#ec4899",
+        },
+      },
+    ],
+  },
+plugins: [require("daisyui")],
 }
