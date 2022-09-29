ad64c9b8f055f9f149667a79714d5db2b097ca78
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-09-29 09:45:16 +0900

    annotate実行

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: contents
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_contents_on_title  (title) UNIQUE
+#
 class Content < ApplicationRecord
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: favorites
+#
+#  id         :bigint           not null, primary key
+#  decade     :integer          not null
+#  gender     :integer          not null
+#  job        :integer
+#  name       :string           not null
+#  type       :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  content_id :bigint
+#
+# Indexes
+#
+#  index_favorites_on_content_id  (content_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (content_id => contents.id)
+#
 class Favorite < ApplicationRecord
   belongs_to :content
 end
