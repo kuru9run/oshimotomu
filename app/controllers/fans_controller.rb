e02d6c85ec0a45ac5a28b0be85fc5e3aafc26bae
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-01 17:16:18 +0900

    推し登録処理実装(まだ動かない)

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -4,4 +4,19 @@ class FansController < ApplicationController
     @contents_titles = Content.select(:title).distinct
     @content = Content.new
   end
+
+  def create
+    @content = Content.find_by(id: favorite_params[:content_id])
+    @favorite = Favorite.find_or_create_by(name: favorite_params[:name], type: favorite_params[:type], decade: favorite_params[:decade], gender: favorite_params[:gender], job: favorite_params[:job], content_id: @content&.id)
+    @fan = Fan.new(user_id: current_user.id, favorite_id: @favorite.id)
+    if @fan.save
+      redirect_to new_fan_path
+    else
+      render :new
+    end
+  end
+
+  def favorite_params
+    params.require(:fan).permit(:name, :type, :decade, :gender, :job, :content_id)
+  end
 end
