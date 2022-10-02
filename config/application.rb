075b1ed241527b9d959495af20661e334a0c85b4
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-02 18:34:14 +0900

    i18n適用させる

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module Oshimotomu
       g.skip_routes true
       g.helper false
     end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
   end
 end
