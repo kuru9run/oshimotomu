75f649c978a0225d305f8b66fedab6836dee1c3b
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-03 21:07:56 +0900

    推し募集詳細画面作成

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,12 @@
 class RequestsController < ApplicationController
   def index
-    @requests = Request.all.includes(:user, :requests_bookmarks)
+    @requests = Request.all.includes(:user)
+  end
+
+  def show
+    @request = Request.find(params[:id])
+    fans_before = @request.user.fans.where(state: :before)
+    @favorites_before = fans_before.map { |fan| fan.favorite }
   end
 
   def new
@@ -8,11 +14,24 @@ class RequestsController < ApplicationController
   end
 
   def create
+    @request = Request.new(request_params)
+    @request.user = current_user
+    if @request.save
+      redirect_to requests_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:title, :description, :existence, :decade, :gender, :job)
   end
 end
