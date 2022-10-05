c597d3c5d59f00a1661048fc30ff1cece3a0ef26
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-05 21:32:09 +0900

    推し募集に対する回答機能実装

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,22 @@
+class AnswersController < ApplicationController
+  def create
+    @answer = current_user.answers.build(answer_params)
+    if @answer.save
+      redirect_to request_path(@answer.request)
+    else
+      render 'requests#show'
+    end
+  end
+
+  def destroy
+    answer = Answer.find(params[:id])
+    answer.destroy!
+    redirect_to request_path(answer.request)
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body).merge(request_id: params[:request_id])
+  end
+end
