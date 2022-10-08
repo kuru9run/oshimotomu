0552730f1181f59d41cc2e04ca2a4b46b874d528
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-08 14:44:37 +0900

    ログイン後のリダイレクト先をマイページに変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to(requests_path, notice: 'ログインしました')
+      redirect_to(mypage_path, notice: 'ログインしました')
     else
       flash.now[:alert] = 'ログインに失敗しました'
       render :new
