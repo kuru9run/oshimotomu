c0f4a6fce401282caafe780cfe0874724c896d10
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-08 21:33:37 +0900

    プロフィールを表示

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,4 +1,6 @@
 class MypagesController < ApplicationController
   def show
+    @now_favorites = current_user.fans.now.map { |fan| fan.favorite }
+    @before_favorites = current_user.fans.before.map { |fan| fan.favorite }
   end
 end
