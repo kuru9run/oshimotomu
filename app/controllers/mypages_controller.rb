95229b6defe8a4f0438b0444ffd87ab85d0da3fa
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-09 10:57:19 +0900

    マイページに投稿一覧を表示させる

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,5 +2,9 @@ class MypagesController < ApplicationController
   def show
     @now_favorites = current_user.fans.now.map { |fan| fan.favorite }
     @before_favorites = current_user.fans.before.map { |fan| fan.favorite }
+    @wrote_requests = current_user.requests
+    @answered_requests = current_user.answers.map { |answer| answer.request }
+    @wrote_promotions = current_user.promotions
+    @commented_promotions = current_user.comments.map { |comment| comment.promotion }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,8 @@ class User < ApplicationRecord
   has_many :requests, dependent: :destroy
   has_many :request_bookmarks, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :promotions, dependent: :destroy
+  has_many :promotion_bookmarks, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   validates :password, length: { minimum: 3 }, confirmation: true, if: -> { new_record? || change[:crypted_password] }
