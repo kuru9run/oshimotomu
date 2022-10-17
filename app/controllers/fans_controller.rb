d4ed7bbf47ca7d9878efbbca789a93af82d88946
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-17 12:01:52 +0900

    Improve:推しを追加したら一覧を更新

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -12,11 +12,14 @@ class FansController < ApplicationController
     @favorite = Favorite.find_or_create_by(name: favorite_params[:name], existence: favorite_params[:existence], decade: favorite_params[:decade], gender: favorite_params[:gender], job: favorite_params[:job], content_id: @content&.id)
     @fan = @favorite.fans.build(user_id: current_user.id)
     @fan[:state] = favorite_params[:state]
+    @fans_now = current_user.fans.now.includes(:favorite)
+    @fans_before = current_user.fans.before.includes(:favorite)
     if @favorite.persisted?
       if @fan.save
-        redirect_to new_fan_path
+        @request = request.headers[:HTTP_TURBO_FRAME]
+        #redirect_to new_fan_path
       else
-        flash.now[:danger] = t('.fail')
+        flash.now[:alert] = t('.fail')
         render :new, status: :unprocessable_entity
       end
     end
