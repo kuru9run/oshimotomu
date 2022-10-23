5e2069ca046916042a8f0d9fcf50f4ca02d21c19
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-10-23 15:07:17 +0900

    Add:font-awesome導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "chartkick/chart.js"
 import "./controllers"
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import '@fortawesome/fontawesome-free'
+library.add(fas, far, fab)
