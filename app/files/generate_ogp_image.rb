eec6b144b2e33b34960c354cc3d0895e456f1d7b
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-12-08 10:07:19 +0900

    Add: 推し募集も動的OGPに対応

--- app/files/generate_ogp_image.rb
+++ app/files/generate_ogp_image.rb
@@ -1,10 +1,10 @@
 class GenerateOgpImage
   BASE_IMAGE_PATH = './app/assets/images/ogp_template.png'.freeze
   FONT_PATH = './app/assets/font/ZenMaruGothic-Regular.ttf'.freeze
-  GRAVITY = 'center'.freeze
-  TEXT_POSITION = '0.0'.freeze
+  GRAVITY_TITLE = 'center'.freeze
+  TEXT_POSITION_TITLE = '0.0'.freeze
   COLOR_CODE = '#592960'.freeze
-  FONT_SIZE = 48
+  FONT_SIZE_TITLE = 48
   INDENTION_COUNT = 15
   ROW_LIMIT = 20
 
@@ -25,10 +25,10 @@ class GenerateOgpImage
     # 画像に投稿タイトルを埋め込み
     image.combine_options do |config|
       config.font FONT_PATH
-      config.gravity GRAVITY
-      config.pointsize FONT_SIZE
+      config.gravity GRAVITY_TITLE
+      config.pointsize FONT_SIZE_TITLE
       config.fill COLOR_CODE
-      config.annotate  "#{TEXT_POSITION}", "#{@text}"
+      config.annotate  "#{TEXT_POSITION_TITLE}", "#{@text}"
     end
   end
 end
