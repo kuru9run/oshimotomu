368c8eab94a9814a3d2a0cb2c88b36218d6d3de0
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-12-10 17:12:00 +0900

    Add: 認可されていない場合はトップにリダイレクトさせる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, alert: t('defaults.message.unauthorized')
+  end
+
   private
   def not_authenticated
     redirect_to main_app.login_path, alert: t('defaults.message.require_login')
