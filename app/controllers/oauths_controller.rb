48e925e625857d387d5e618042a896b984ba50b5
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-12-13 14:11:06 +0900

    Add: oauthとロールバックの処理追加

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,31 @@
+class OauthsController < ApplicationController
+  skip_before_action :require_login, raise: false
+
+  def oauth
+    login_at(auth_params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+    debugger
+    if @user = login_from(provider)
+      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+    else
+      begin
+        @user = create_from(provider)
+        # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
+
+        reset_session # protect from session fixation attack
+        auto_login(@user)
+        redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+      rescue
+        redirect_to root_path, alert: "#{provider.titleize}でのログインに失敗しました"
+      end
+    end
+  end
+
+  private
+  def auth_params
+    params.permit(:code, :provider, :denied)
+  end
+end
