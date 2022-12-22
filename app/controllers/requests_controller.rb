4c37b33aeb9273e5f527cb81e241385f603b38d5
kuru0901 <nyo0901wa@yahoo.co.jp>
2022-12-22 09:44:30 +0900

    Fix: アバター登録有無の考慮

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -35,7 +35,7 @@ class RequestsController < ApplicationController
 
   def update
     if @request.update(request_params)
-      redirect_to request_path(@request)c
+      redirect_to request_path(@request)
     else
       flash.now[:alert] = t('.fail')
       render :edit, status: :unprocessable_entity
