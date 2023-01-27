36705b6245972529bc417eeccdf8d40368a0cf84
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-01-27 09:20:36 +0900

    Add: 他ユーザーから見たプロフィールページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :require_login
+  before_action :set_user, only: %i[show]
 
   def new
     @user = User.new
@@ -16,7 +17,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @now_favorites = @user.fans.includes(:favorite).now.map { |fan| fan.favorite }
+    @before_favorites = @user.fans.includes(:favorite).before.map { |fan| fan.favorite }
+    @wrote_requests = @user.requests
+    answered_requests = @user.answers.includes(:request).map { |answer| answer.request }
+    @answered_requests = answered_requests.uniq
+    @wrote_promotions = @user.promotions
+    commented_promotions = @user.comments.includes(:promotion).map { |comment| comment.promotion }
+    @commented_promotions = commented_promotions.uniq
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'terms_of_service', to: 'static_pages#terms_of_service'
   resource :mypage, only: %i[show edit update]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
