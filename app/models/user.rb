2f7dbb9bbd968ec13ce4aaafc4cc2a8ae2273493
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-01-28 20:06:48 +0900

    Add: フォロー・アンフォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,11 @@ class User < ApplicationRecord
   has_many :promotion_bookmarks, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :authentications, dependent: :destroy
+  has_many :relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  # 一覧画面用
+  has_many :followings, through: :relationships, source: :follow
+  has_many :followers, through: :reverse_of_relationships, source: :followed
 
   accepts_nested_attributes_for :authentications # has_many :authenticationsより下に書く
 
@@ -51,5 +56,17 @@ class User < ApplicationRecord
 
   def avatar_url
     avatar? ? avatar.url : icon_url
+  end
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'terms_of_service', to: 'static_pages#terms_of_service'
   resource :mypage, only: %i[show edit update]
-  resources :users, only: %i[new create show]
+  resources :users, only: %i[new create show] do
+    resource :relationships, only: %i[create destroy]
+    get 'followings', to: 'relationships#followings', as: 'followings'
+    get 'followers', to: 'relationships#followers', as: 'followers'
+  end
   resources :follows, only: %i[new create destroy]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
