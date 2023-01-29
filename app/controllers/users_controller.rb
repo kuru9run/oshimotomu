98dc41e60a784c0521d70c5cd857ed39c452ee7f
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-01-29 21:28:10 +0900

    Fix: ユーザー詳細画面はログイン必須にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login
+  skip_before_action :require_login, only: %i[new create]
   before_action :set_user, only: %i[show]
 
   def new
