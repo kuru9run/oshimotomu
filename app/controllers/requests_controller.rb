81cb2405863be5667f00a5b7973d56b066516ee1
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-01-29 13:47:35 +0900

    Add: 推し募集にフォロー中の投稿で絞り込み機能追加

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,7 +3,13 @@ class RequestsController < ApplicationController
   before_action :set_request, only: %i[edit update destroy]
 
   def index
-    @q = Request.ransack(params[:q])
+    # フォロー中ユーザーの投稿で絞り込み
+    requests = if params[:follow] == "on"
+      Request.where(user_id: current_user.followings.ids)
+    else
+      Request
+    end
+    @q = requests.ransack(params[:q])
     @requests = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
