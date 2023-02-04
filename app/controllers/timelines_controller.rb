f97eeee5c2f8338f27b5b83847aabc67ed789c43
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-02-04 16:05:15 +0900

    Fix: タイムラインも作成日で降順にする

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -6,11 +6,11 @@ class TimelinesController < ApplicationController
 
   def requests
     ids = Request.includes(:user).select{ |x| current_user.following?(x.user) }.pluck(:id)
-    @requests = Request.where(id: ids).includes(:user).page(params[:page])
+    @requests = Request.where(id: ids).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def promotions
     ids = Promotion.includes(:user).select{ |x| current_user.following?(x.user) }.pluck(:id)
-    @promotions = Promotion.where(id: ids).includes(:user).page(params[:page])
+    @promotions = Promotion.where(id: ids).includes(:user, :content, :group).order(created_at: :desc).page(params[:page])
   end
 end
