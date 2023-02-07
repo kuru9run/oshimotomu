3f6fe58ca0dc3583387ca578a314fcc128f0cabb
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-02-07 19:55:50 +0900

    Add: 各ページタイトルをタブに表示し、OGPと合わせてヘルパーに切り出し

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def page_title(page_title = '')
+    base_title = '求：推し'
+    page_title.empty? ? base_title : page_title + '  ･*' + base_title + '⋆｡ '
+  end
+
+  def ogp_title(ogp_title = '')
+    base_title = '求：推し'
+    ogp_title.empty? ? base_title : ogp_title + '  ･*' + base_title + '⋆｡ '
+  end
+
+  def ogp_image(ogp_image = '')
+    base_image = "ogp_default.png"
+    ogp_image.empty? ? base_image : ogp_image
+  end
 end
