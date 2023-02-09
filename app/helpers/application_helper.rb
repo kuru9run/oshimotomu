2da7ff7b9c35505ec8dc8e6838dbf83a32807a9a
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-02-09 18:36:19 +0900

    Improve: ページタイトルのヘルパーの書き方をシンプルにリファクタ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   def page_title(page_title = '')
-    base_title = '求：推し'
-    page_title.empty? ? base_title : page_title + '  ･*' + base_title + '⋆｡ '
+    base_title = ' ･*求：推し⋆｡ '
+    page_title.empty? ? base_title : page_title + base_title
   end
 
   def og_title(og_title = '')
-    base_title = '求：推し'
-    og_title.empty? ? base_title : og_title.gsub(' ', '') + '･*' + base_title + '⋆｡'
+    base_title = '･*求：推し⋆｡'
+    og_title.empty? ? base_title : og_title.gsub(' ', '') + base_title
   end
 
   def ogp_image(ogp_image = '')
