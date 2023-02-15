5056d6d24990fd6433b799e10110420a5715681c
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-02-15 10:40:13 +0900

    Add: 布教の投稿に関するモデルスペック追加

---
+++ spec/models/promotion_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Promotion, type: :model do
+  let(:promotion) { build(:promotion) }
+  it "既定文字数内での投稿が成功すること" do
+    expect(promotion).to be_valid
+  end
+  it "推し氏名が空白のままで投稿できないこと" do
+    promotion.name = ''
+    expect(promotion).to_not be_valid
+  end
+  it "布教内容が65535文字を超えている投稿が失敗すること" do
+    promotion.description = Faker::Lorem.characters(number: 65_536)
+    expect(promotion).to_not be_valid
+  end
+end
