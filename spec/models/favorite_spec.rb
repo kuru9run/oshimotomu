45c5b7173597bf8665f4bd93a54a5e6b942c456e
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-02-19 17:13:47 +0900

    Add: 推し登録のモデルスペックを追加

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+  let(:favorite_person) { build(:favorite_person) }
+  let(:favorite_character) { build(:favorite_character) }
+  it "推し（実在する人物）が登録できること" do
+    expect(favorite_person).to be_valid
+  end
+  it "推し（キャラクター）が登録できること" do
+    expect(favorite_character).to be_valid
+  end
+  it "推し氏名が空白のままで登録できないこと" do
+    favorite_person.name = ''
+    expect(favorite_person).to_not be_valid
+  end
+  it "推し氏名が255文字を超えて登録できないこと" do
+    favorite_person.name = Faker::Lorem.characters(number: 256)
+    expect(favorite_person).to_not be_valid
+  end
+end
