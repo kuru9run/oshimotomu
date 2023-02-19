f3fb7280c8aff161afa16161c5df1b8d1893d7c8
kuru0901 <nyo0901wa@yahoo.co.jp>
2023-02-19 15:13:37 +0900

    Add: 回答の投稿に関するモデルスペック追加

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:answer) { build(:answer) }
+  it "既定文字数内での投稿が成功すること" do
+    expect(answer).to be_valid
+  end
+  it "本文が空白のままで投稿できないこと" do
+    answer.body = ''
+    expect(answer).to_not be_valid
+  end
+  it "本文が65535文字を超えている投稿が失敗すること" do
+    answer.body = Faker::Lorem.characters(number: 65_536)
+    expect(answer).to_not be_valid
+  end
+end
